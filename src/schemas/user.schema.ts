9cf7d2f0a9443401f742b3be7c1f91de47fa9b09
Theus <matheusth.dev@gmail.com>
2023-06-11 20:59:54 -0300

    fix: tamanho máximo do campo de nome na criação de usuário

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,7 +2,11 @@ import * as yup from "yup";
 import { IRecoverPasswordRequest, IUserRequest } from "../types/user";
 
 export const createUserSchema: yup.SchemaOf<IUserRequest> = yup.object().shape({
-  name: yup.string().required("name: campo obrigatório.").min(4).max(30),
+  name: yup
+    .string()
+    .required("name: campo obrigatório.")
+    .min(4, "name: mínimo de 4 caracteres")
+    .max(30, "name: máximo de 30 caracteres"),
 
   password: yup
     .string()
